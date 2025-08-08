14afceb9739d9869ae677ec7c0139618611b0b64
Dullat <142017979+Dullat@users.noreply.github.com>
2025-08-08 19:07:30 +0530

    sidebar fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,42 @@
 import { Link, NavLink } from "react-router-dom"
 import { FaGlobe, FaBars, FaUser, FaTimes } from "react-icons/fa"
-import { useContext, useState } from "react"
+import { useContext, useRef, useState, useEffect } from "react"
 import { AuthContext } from "../context/AuthProvider"
 import { useLocation, matchPath } from "react-router-dom"
 
 const NavBar = () => {
-    const [isOpen, setIsOpen] = useState(true)
+    const [isOpen, setIsOpen] = useState(false)
     const { userProfile } = useContext(AuthContext)
     const { user } = useContext(AuthContext)
     const location = useLocation()
+    const asideRef = useRef(null)
+    const burgerRef = useRef(null)
 
     const isOnPath = matchPath('/login', location.pathname)
+
+    useEffect(() => {
+        if (!isOpen) return;
+
+        function handleOutsideClick(e) {
+            if (!asideRef.current || !burgerRef.current) return;
+            if(burgerRef.current.contains(e.target)) return
+            if (
+                
+                e.target.contains(asideRef.current) // true : asideRef.current.contains(asideRef.current) -> true
+            ) {     
+                console.log('hahah')          
+                return;
+            }else{
+
+                console.log('setting false');
+                
+            setIsOpen(false);
+            }
+        }
+        window.addEventListener('click', handleOutsideClick);
+        return () => window.removeEventListener('click', handleOutsideClick);
+    }, [isOpen]);
+
     return (
         <div className='text-white flex justify-between items-center p-4 bg-[#0e0e10]'>
             <div className="text-2xl">Dullat Store</div>
@@ -21,6 +47,7 @@ const NavBar = () => {
                 <Avatar userProfile={userProfile} user={user} isOnPath={isOnPath} />
             </div>
             <button
+                ref={burgerRef}
                 className="md:hidden text-gray-100 cursor-pointer"
                 onClick={() => setIsOpen(true)}
                 aria-label="Open menu"
@@ -34,7 +61,7 @@ const NavBar = () => {
                             className="fixed inset-0 z-30 bg-black/40 backdrop-blur-[1px] transition-opacity md:hidden"
                             onClick={() => setIsOpen(false)}
                         />
-                        <aside className="md:hidden fixed top-0 right-0 w-72 max-w-full h-full z-40 bg-[#1f1f29] shadow-xl border-l border-zinc-800 flex flex-col p-6 animate-slide-in">
+                        <aside ref={asideRef} className="md:hidden fixed top-0 right-0 w-72 max-w-full h-full z-40 bg-[#1f1f29] shadow-xl border-l border-zinc-800 flex flex-col p-6 animate-slide-in">
                             <button
                                 className="self-end text-gray-300 mb-6 cursor-pointer"
                                 onClick={() => setIsOpen(false)}
@@ -70,7 +97,7 @@ const Nav = ({ className }) => {
 }
 
 const Avatar = ({ userProfile, user, isOnPath, fromSideBar = false }) => {
-    const {signOut} = useContext(AuthContext)
+    const { signOut } = useContext(AuthContext)
 
     return (
         <div className="gap-6 flex items-center w-full mt-auto">
