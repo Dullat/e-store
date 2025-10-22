eb7c368ba49eeefe368449c38d2a7c7170fde201
dullat <142017979+Dullat@users.noreply.github.com>
2025-10-22 22:24:00 +0530

    setup refresh restore profile

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Header from '../components/Header'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
+import React from "react";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+
+import UseAuthListner from "../app/UseAuthListner.jsx";
 
 const RootLayout = () => {
+  UseAuthListner();
   return (
-    <main className='min-h-screen bg-gradient-to-b from-[#0f0f0f] via-[#1c1c1c] to-[#111] text-white flex flex-col'>
+    <main className="min-h-screen bg-gradient-to-b from-[#0f0f0f] via-[#1c1c1c] to-[#111] text-white flex flex-col">
       <Header />
-      <div className='min-h-max mb-8'>
+      <div className="min-h-max mb-8">
         <Outlet />
       </div>
       <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
+
