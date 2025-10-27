be12dd7c74fa4a59a0e00d230911f9430d44b0a4
dullat <142017979+Dullat@users.noreply.github.com>
2025-10-27 21:51:46 +0530

    add NewsPage, use .md here for now

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import about from "../content/about.md?raw";
 
 const NewsPage = () => {
   return (
-    <div>
-        not developed yet..........
+    <div className="prose dark:prose-invert max-w-4xl mx-auto px-4 py-10">
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{about}</ReactMarkdown>
     </div>
-  )
-}
+  );
+};
 
-export default NewsPage
+export default NewsPage;
+
